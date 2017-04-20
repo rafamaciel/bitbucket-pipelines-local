e35abb005348c69c82ad54d164c0b435786edd89
Rafael de Barcelos Maciel <rafamaciel@MacBook-Pro-de-Rafael.local>
2017-04-20 15:17:46 -0300

    Add class to load the entire module

---
+++ lib/bitbucket_pipelines_local/all.rb
@@ -0,0 +1,8 @@
+require "bitbucket_pipelines_local"
+require "bitbucket_pipelines_local/version"
+require "bitbucket_pipelines_local/globber"
+require "bitbucket_pipelines_local/config"
+require "bitbucket_pipelines_local/docker"
+
+require "tty-command"
+require "tty-spinner"
